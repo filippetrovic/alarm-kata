e0619682dfb2b3735288f986685b3da494ce2ded
Filip Petrovic <fpetrovic92@gmail.com>
2021-03-29 12:23:47 +0200

    Realistic behaviour in email service

--- src/main/java/kata/alarm/EmailService.java
+++ src/main/java/kata/alarm/EmailService.java
@@ -3,7 +3,11 @@ package kata.alarm;
 public class EmailService {
 
   public void sendWarningEmail(int elapsed) {
-    System.out.println("Warning! Elapsed minutes: " + elapsed);
+    try {
+      Thread.sleep(5000);
+      throw new RuntimeException("Can't reach email server.");
+    } catch (InterruptedException ignored) {
+    }
   }
 
 }
